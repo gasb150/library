53fc6ee52c9beb9157949cc28fea57e81f153d4a
tavo <gasb_150@hotmail.com>
2021-03-19 15:43:59 -0500

    fix linters

--- assets/Script/function.js
+++ assets/Script/function.js
@@ -1,25 +1,25 @@
 
-export const functionalities = (()=>{
- const storageMyLibrary = (myLibrary) => {
-  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-}
+export const functionalities = (() => {
+  const storageMyLibrary = (myLibrary) => {
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+  };
 
- const  remove = (id) => {
-  id = id.slice(6);
-  const myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
-  myLibrary.splice(id, 1);
-  storageMyLibrary(myLibrary);
-}
-const reloadPage = () => {
-  window.location.reload();
-}
-return{
-  storageMyLibrary,
-  reloadPage,
-  remove,
-}
-})()
- export default function Book(title, author, pages, read) {
+  const remove = (id) => {
+    id = id.slice(6);
+    const myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+    myLibrary.splice(id, 1);
+    storageMyLibrary(myLibrary);
+  };
+  const reloadPage = () => {
+    window.location.reload();
+  };
+  return {
+    storageMyLibrary,
+    reloadPage,
+    remove,
+  };
+})();
+export default function Book(title, author, pages, read) {
   this.author = author;
   this.title = title;
   this.pages = pages;
